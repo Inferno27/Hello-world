0c9dfaecc2cee63f4b82182c861530c97e098396
Inferno27 <tahirjamie@gmail.com>
2016-06-30 14:48:59 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        system.out.println("Added more code");
-system.out.println("Added more code twice");
+       
     }
 
 }
